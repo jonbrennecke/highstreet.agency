e2c84b57f862d433f0bbdc97a7e1e6563ab4558f
Jon Brennecke <jpbrennecke@gmail.com>
2018-11-27 13:14:40 -0800

    added navigation links

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react';
 import classnames from 'classnames';
+import { NavLink } from 'react-router-dom';
 
 import NavLogo from '../nav-logo/NavLogo';
 
@@ -18,6 +19,20 @@ export default function Navigation({ className, analytics }: Props) {
   return (
     <nav className={classnames('navigation', className)}>
       <NavLogo />
+      <div className="nav-links">
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/process" className="nav-link" activeClassName="active">
+          Process
+        </NavLink>
+        <NavLink to="/work" className="nav-link" activeClassName="active">
+          Work
+        </NavLink>
+        <NavLink to="/contact" className="nav-link" activeClassName="active">
+          Contact Us
+        </NavLink>
+      </div>
     </nav>
   );
 }
