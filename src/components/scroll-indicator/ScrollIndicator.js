1964601d87b9d27aaea09c7e5d158e30fd7d1c39
Jon Brennecke <jpbrennecke@gmail.com>
2018-11-26 22:36:42 -0800

    css animation for scroll indicator

--- src/components/scroll-indicator/ScrollIndicator.js
+++ src/components/scroll-indicator/ScrollIndicator.js
@@ -57,49 +57,17 @@ export default class ScrollIndicator extends Component<Props, State> {
         className={classnames(
           'scroll-indicator',
           this.props.className,
-          this.state.isVisible && 'visible'
+          this.state.isVisible && 'visible',
+          'animated' // TODO
         )}
         style={{
           marginBottom: this.state.pageYOffset / 2
         }}
       >
-        <svg
-          width="46px"
-          height="72px"
-          viewBox="0 0 46 72"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xlinkHref="http://www.w3.org/1999/xlink"
-        >
-          <g
-            id="Website-Marketting-Page"
-            stroke="none"
-            strokeWidth="1"
-            fill="none"
-            fillRule="evenodd"
-            transform="scale(0.8)"
-          >
-            <g
-              id="Home-page"
-              transform="translate(-697.000000, -745.000000)"
-              fillRule="nonzero"
-              stroke="#4259D9"
-              strokeWidth="4"
-            >
-              <g id="Mouse" transform="translate(697.000000, 745.000000)">
-                <path d="M23,16 L23,26" id="Line-2" strokeLinecap="round" />
-                <rect
-                  id="Rectangle"
-                  x="2"
-                  y="2"
-                  width="42"
-                  height="68"
-                  rx="21"
-                />
-              </g>
-            </g>
-          </g>
-        </svg>
+        <div id="mouse-outline">
+          <div id="mouse-wheel"/>
+        </div>
+        <h1 className="scroll-text">scroll</h1>
       </div>
     );
   }
