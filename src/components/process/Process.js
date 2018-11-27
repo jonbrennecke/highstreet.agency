3e06e979a1ba9815c928c722b73e47a3e55620f9
Jon Brennecke <jpbrennecke@gmail.com>
2018-11-26 16:24:07 -0800

    added basic scroll indicator that disappears on scroll

--- src/components/process/Process.js
+++ src/components/process/Process.js
@@ -8,6 +8,7 @@ import './Process.scss';
 import ContentMaxWidth from '../layout/content-max-width/ContentMaxWidth';
 import Subtitle from '../subtitle/Subtitle';
 import Button from '../button/Button';
+import ScrollIndicator from '../scroll-indicator/ScrollIndicator';
 
 import type { Analytics } from '../../utils/Analytics';
 
@@ -20,16 +21,19 @@ export default function Process({ className, analytics }: Props) {
   return (
     <section className={classnames('process', className)}>
       <ContentMaxWidth className="process-inner">
-        <Subtitle text="We work with startups, brands and founders to imagine new experiences for mobile, desktop and beyond." />
-        <Button
-          analytics={analytics}
-          className="call-to-action-button"
-          name="Process call-to-action button"
-          text="See our process"
-          onClick={() => {
-            // TODO animate page transition to /projects
-          }}
-        />
+        <ScrollIndicator />
+        <div className="below-scroll-content">
+          <Subtitle text="We work with startups, founders and brands to create new experiences for mobile, desktop and beyond." />
+          <Button
+            analytics={analytics}
+            className="call-to-action-button"
+            name="Process call-to-action button"
+            text="See our process"
+            onClick={() => {
+              // TODO animate page transition to /projects
+            }}
+          />
+        </div>
       </ContentMaxWidth>
     </section>
   );
---
+++ src/components/scroll-indicator/ScrollIndicator.js
@@ -0,0 +1,94 @@
+// @flow
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { autobind } from 'core-decorators';
+
+// $FlowFixMe
+import './ScrollIndicator.scss';
+
+type Props = {
+  className?: ?string,
+};
+
+type State = {
+  isVisible: boolean,
+};
+
+// $FlowFixMe
+@autobind
+export default class ScrollIndicator extends Component<Props, State> {
+  state = {
+    isVisible: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScrollEvent);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScrollEvent);
+  }
+
+  handleScrollEvent() {
+    window.requestAnimationFrame(() => {
+      this.onScroll();
+    });
+  }
+
+  onScroll() {
+    if (window.pageYOffset > 45) {
+      this.setState({
+        isVisible: false
+      });
+    }
+    else {
+      this.setState({
+        isVisible: true
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className={classnames('scroll-indicator', this.props.className, this.state.isVisible && 'visible')}>
+        <svg
+          width="46px"
+          height="72px"
+          viewBox="0 0 46 72"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xlinkHref="http://www.w3.org/1999/xlink"
+        >
+          <g
+            id="Website-Marketting-Page"
+            stroke="none"
+            strokeWidth="1"
+            fill="none"
+            fillRule="evenodd"
+            transform="scale(0.8)"
+          >
+            <g
+              id="Home-page"
+              transform="translate(-697.000000, -745.000000)"
+              fillRule="nonzero"
+              stroke="#4259D9"
+              strokeWidth="4"
+            >
+              <g id="Mouse" transform="translate(697.000000, 745.000000)">
+                <path d="M23,16 L23,26" id="Line-2" strokeLinecap="round" />
+                <rect
+                  id="Rectangle"
+                  x="2"
+                  y="2"
+                  width="42"
+                  height="68"
+                  rx="21"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    );
+  }
+}
