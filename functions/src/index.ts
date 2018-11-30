1be50eb110711522f1db4368239f0a23cfabd273
Jon Brennecke <jpbrennecke@gmail.com>
2018-11-30 14:51:45 -0800

    added slack webhook to cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as cors from 'cors';
+import * as rp from 'request-promise';
 
 const corsMiddleware = cors({ origin: true });
 
@@ -10,6 +11,7 @@ const users = db.ref('users');
 
 export const createContactFormRecord = functions.https.onRequest((req, res) => {
   return corsMiddleware(req, res, async () => {
+    await postToSlack(req.body.name);
     await users.push({
       name: req.body.name,
       email: req.body.email,
@@ -25,3 +27,15 @@ export const createContactFormRecord = functions.https.onRequest((req, res) => {
     });
   });
 });
+
+// See: https://github.com/firebase/functions-samples/blob/Node-8/github-to-slack/functions/index.js
+function postToSlack(name: string) {
+  return rp({
+    method: 'POST',
+    uri: functions.config().slack.webhook_url,
+    body: {
+      text: `${name} submitted a contact request`,
+    },
+    json: true,
+  });
+}
