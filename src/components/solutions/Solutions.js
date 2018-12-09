59a895bd9fa4a71f8bc8d105feaf4750361d645c
Jon Brennecke <jpbrennecke@gmail.com>
2018-12-09 14:16:15 -0800

    added animated containers to solutions section

--- src/components/solutions/Solutions.js
+++ src/components/solutions/Solutions.js
@@ -6,6 +6,7 @@ import Feature from '../feature/Feature';
 import Section from './Section';
 import InterfaceFigure from './InterfacesFigure';
 import AutomationImage from './AutomationImage';
+import AnimateUpOnScroll from '../animated/AnimateUpOnScroll';
 
 import type { Analytics } from '../../utils/Analytics';
 
@@ -17,26 +18,32 @@ type Props = {
 export default function Solutions({ className, analytics }: Props) {
   return (
     <div className={classnames('solutions', className)}>
-      <Section
-        className={classnames('solutions-ui', className)}
-        analytics={analytics}
-        name="Development"
-        title="DEVELOPMENT"
-        text="Our engineers will help you reach new audiences with top-notch software for every platform."
-        buttonText="See our development process"
-        renderFigure={() => <InterfaceFigure />}
-      />
-      <Feature analytics={analytics} />
-      <Section
-        className={classnames('solutions-automation', className)}
-        isReversed={true}
-        analytics={analytics}
-        name="Automation"
-        title="AUTOMATION"
-        text="Our experienced DevOps team will scale your platform with strategies for streamlined automation, continuous integration and deployment."
-        buttonText="Learn more"
-        renderFigure={() => <AutomationImage />}
-      />
+      <AnimateUpOnScroll className="animated-container">
+        <Section
+          className={classnames('solutions-ui', className)}
+          analytics={analytics}
+          name="Development"
+          title="DEVELOPMENT"
+          text="Our engineers will help you reach new audiences with top-notch software for every platform."
+          buttonText="See our development process"
+          renderFigure={() => <InterfaceFigure />}
+        />
+      </AnimateUpOnScroll>
+      <AnimateUpOnScroll className="animated-container">
+        <Feature analytics={analytics} />
+      </AnimateUpOnScroll>
+      <AnimateUpOnScroll className="animated-container">
+        <Section
+          className={classnames('solutions-automation', className)}
+          isReversed={true}
+          analytics={analytics}
+          name="Automation"
+          title="AUTOMATION"
+          text="Our experienced DevOps team will scale your platform with strategies for streamlined automation, continuous integration and deployment."
+          buttonText="Learn more"
+          renderFigure={() => <AutomationImage />}
+        />
+      </AnimateUpOnScroll>
     </div>
   );
 }
